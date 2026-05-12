303fa6db7e3ba6febc5e644dc1b273f71e54f651
Frandium <frandium@163.com>
2026-05-12 12:00:03 +0800

    update task results

--- fdong/scripts/extract_train_log.py
+++ fdong/scripts/extract_train_log.py
@@ -7,7 +7,7 @@ from statistics import mean
 
 # Edit this block before each experiment, then run:
 #   python fdong/scripts/extract_train_log.py
-EXPERIMENT_NAME = "unet-4"
+EXPERIMENT_NAME = "unet-4-8-4"
 LOG_FILE = f"../logs/{EXPERIMENT_NAME}.log"
 OUTPUT_DIR = f"../experiments/{EXPERIMENT_NAME}"
 RETRIEVED_LINES = 100
